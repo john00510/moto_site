b2fb04d8879b075fd13c6ce09ce938d24266228f
yuri <yury0051@gmail.com>
2018-03-30 20:43:23 +0300

    m

---
+++ client/src/pages/HomePage/Components/PopularMakes.js
@@ -0,0 +1,139 @@
+<h2>Popular Used Cars for Sale</h2>
+<div class="link-section">
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/honda">Honda</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/honda/cr-v">CR-V</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/honda/civic">Civic</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/honda/accord">Accord</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/honda/pilot">Pilot</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/toyota">Toyota</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/toyota/highlander">Highlander</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/toyota/camry">Camry</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/toyota/tacoma">Tacoma</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/toyota/4runner">4runner</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/jeep">Jeep</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/jeep/wrangler">Wrangler</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/jeep/renegade">Renegade</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/jeep/grand-cherokee">Grand Cherokee</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/jeep/cherokee">Cherokee</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/ford">Ford</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/ford/explorer">Explorer</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/ford/escape">Escape</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/ford/f150">F150</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/ford/focus">Focus</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/dodge">Dodge</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/dodge/challenger">Challenger</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/dodge/charger">Charger</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/dodge/durango">Durango</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/dodge/journey">Journey</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/hyundai">Hyundai</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/hyundai/sonata">Sonata</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/hyundai/santa-fe">Santa Fe</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/hyundai/genesis">Genesis</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/hyundai/elantra">Elantra</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/subaru">Subaru</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/subaru/wrx">WRX</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/subaru/forester">Forester</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/subaru/outback">Outback</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/subaru/impreza">Impreza</a>
+    </li>
+  </ul>
+  <ul>
+    <li class="link-make">
+      <a href="https://www.carmax.com/cars/chevrolet">Chevrolet</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/chevrolet/cruze">Cruze</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/chevrolet/colorado">Colorado</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/chevrolet/equinox">Equinox</a>
+    </li>
+    <li>
+      <a href="https://www.carmax.com/cars/chevrolet/tahoe">Tahoe</a>
+    </li>
+  </ul>
+</div>
---
+++ client/src/pages/HomePage/Components/ResentlyViewedVehicles.js
@@ -0,0 +1,138 @@
+<div data-reactroot="">
+  <div class="inventory-band content">
+    <h2 class="kmx-typography-display-2">Recently Viewed</h2>
+    <div class="">
+      <div class="carousel show-arrows">
+        <div class="slick-initialized slick-slider">
+          <button type="button" data-role="none" class="slick-arrow slick-prev slick-disabled" style="display: block;">
+            Previous</button>
+          <div class="slick-list">
+            <div class="slick-track" style="opacity: 1; transform: translate3d(0px, 0px, 0px); width: 4212px;">
+              <div data-index="0" class="slick-slide slick-active" tabindex="-1" style="outline: medium none currentcolor;">
+                <a href="https://www.carmax.com/car/15415286" class="car-tile">
+                  <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342_002.jpg">
+                    <div class="car-tile--info-container">
+                      <div class="car-tile--heading-container">
+                        <h3 class="car-tile--heading">2015 Acura RDX</h3>
+                      </div>
+                      <div class="car-tile--sub-heading-container">
+                        <span class="">$27,998</span>
+                        <sup>*</sup>
+                        <span class="separator">•</span>
+                        <span>18K</span>
+                      </div>
+                      <div class="car-tile--sub-heading-container car-tile--location">Seattle, WA</div>
+                    </div>
+                  </a>
+                </div>
+                <div data-index="1" class="slick-slide slick-active" tabindex="-1" style="outline: medium none currentcolor;">
+                  <a href="https://www.carmax.com/car/15401249" class="car-tile">
+                    <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342.jpg">
+                      <div class="car-tile--info-container">
+                        <div class="car-tile--heading-container">
+                          <h3 class="car-tile--heading">2016 Acura TLX</h3>
+                        </div>
+                        <div class="car-tile--sub-heading-container">
+                          <span class="">$24,599</span>
+                          <sup>*</sup>
+                          <span class="separator">•</span>
+                          <span>32K</span>
+                        </div>
+                        <div class="car-tile--sub-heading-container car-tile--location">Fremont, CA</div>
+                      </div>
+                    </a>
+                  </div>
+                  <div data-index="2" class="slick-slide slick-active" tabindex="-1" style="outline: medium none currentcolor;">
+                    <a href="https://www.carmax.com/car/14945306" class="car-tile">
+                      <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342_004.jpg">
+                        <div class="car-tile--info-container">
+                          <div class="car-tile--heading-container">
+                            <h3 class="car-tile--heading">2017 Acura ILX</h3>
+                          </div>
+                          <div class="car-tile--sub-heading-container">
+                            <span class="">$23,998</span>
+                            <sup>*</sup>
+                            <span class="separator">•</span>
+                            <span>4K</span>
+                          </div>
+                          <div class="car-tile--sub-heading-container car-tile--location">Seattle, WA</div>
+                        </div>
+                      </a>
+                    </div>
+                    <div data-index="3" class="slick-slide" tabindex="-1" style="outline: medium none currentcolor;">
+                      <a href="https://www.carmax.com/car/15593556" class="car-tile">
+                        <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342_003.jpg">
+                          <div class="car-tile--info-container">
+                            <div class="car-tile--heading-container">
+                              <h3 class="car-tile--heading">2015 Audi A3 Premium</h3>
+                            </div>
+                            <div class="car-tile--sub-heading-container">
+                              <span class="">$21,599</span>
+                              <sup>*</sup>
+                              <span class="separator">•</span>
+                              <span>17K</span>
+                            </div>
+                            <div class="car-tile--sub-heading-container car-tile--location">Seattle, WA</div>
+                          </div>
+                        </a>
+                      </div>
+                      <div data-index="4" class="slick-slide" tabindex="-1" style="outline: medium none currentcolor;">
+                        <a href="https://www.carmax.com/car/15755581" class="car-tile">
+                          <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342_006.jpg">
+                            <div class="car-tile--info-container">
+                              <div class="car-tile--heading-container">
+                                <h3 class="car-tile--heading">2013 Chevrolet Equinox LS</h3>
+                              </div>
+                              <div class="car-tile--sub-heading-container">
+                                <span class="">$15,998</span>
+                                <sup>*</sup>
+                                <span class="separator">•</span>
+                                <span>43K</span>
+                              </div>
+                              <div class="car-tile--sub-heading-container car-tile--location">Dallas, TX</div>
+                            </div>
+                          </a>
+                        </div>
+                        <div data-index="5" class="slick-slide" tabindex="-1" style="outline: medium none currentcolor;">
+                          <a href="https://www.carmax.com/car/15639496" class="car-tile">
+                            <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342_007.jpg">
+                              <div class="car-tile--info-container">
+                                <div class="car-tile--heading-container">
+                                  <h3 class="car-tile--heading">2017 Buick LaCrosse Premium</h3>
+                                </div>
+                                <div class="car-tile--sub-heading-container">
+                                  <span class="">$27,998</span>
+                                  <sup>*</sup>
+                                  <span class="separator">•</span>
+                                  <span>7K</span>
+                                </div>
+                                <div class="car-tile--sub-heading-container car-tile--location">San Francisco, CA</div>
+                              </div>
+                            </a>
+                          </div>
+                          <div data-index="6" class="slick-slide" tabindex="-1" style="outline: medium none currentcolor;">
+                            <a href="https://www.carmax.com/car/15312769" class="car-tile">
+                              <img class="car-tile--hero-image" src="CarMax%20-%20Browse%20used%20cars%20and%20new%20cars%20online_files/342_005.jpg">
+                                <div class="car-tile--info-container">
+                                  <div class="car-tile--heading-container">
+                                    <h3 class="car-tile--heading">2016 Audi A3 Premium</h3>
+                                  </div>
+                                  <div class="car-tile--sub-heading-container">
+                                    <span class="">$20,998</span>
+                                    <sup>*</sup>
+                                    <span class="separator">•</span>
+                                    <span>28K</span>
+                                  </div>
+                                  <div class="car-tile--sub-heading-container car-tile--location">Seattle, WA</div>
+                                </div>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                        <button type="button" data-role="none" class="slick-arrow slick-next" style="display: block;">
+                          Next</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
