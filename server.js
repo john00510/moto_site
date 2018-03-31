2efd9b98ae6e7426a5599d80eeaf9a758b72ef63
name <example@example.com>
2018-03-31 17:03:50 +0000

    m

--- server.js
+++ server.js
@@ -12,14 +12,19 @@ app.get('/', (req, res) => {
 
 app.post('/commit_event', (req, res) => {
   var cmd = '/home/john/motosite/update.sh';
+  var url = req.post('host') + req.originalUrl;
 
-  exec(cmd, (err, stdout, stderr) => {
-    if (err) {
-      res.send({ express: stderr });
-    } else {
-      res.send({ express: stdout });
-    }
-  });
+  if (url === '') {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        res.send({ express: stderr });
+      } else {
+        res.send({ express: stdout });
+      }
+    });
+  } else {
+    res.send({ express: 'validation error' });
+  }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
