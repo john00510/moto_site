6ea837e0e4b40193f10dc9d0748584e2406dbd36
name <example@example.com>
2018-03-31 00:59:19 +0000

    m

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
